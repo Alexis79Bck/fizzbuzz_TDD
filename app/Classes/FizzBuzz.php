5da5ab411c2a08a961097b08eb0ee8e22d5fad2a
Alexis E. Mata <alexis.ed.mata@gmail.com>
2023-06-05 17:18:24 -0400

    pruebas cubiertas en fizzbuzztest

--- app/Classes/FizzBuzz.php
+++ app/Classes/FizzBuzz.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Classes;
+use Symfony\Component\Translation\Exception\InvalidArgumentException;
 
 class FizzBuzz
 {
@@ -9,13 +10,45 @@ class FizzBuzz
 
     public function __construct(array $numbers)
     {
-        $this->arrayNumbers = $numbers; 
+        $this->arrayNumbers = $numbers;
+
     }
-    public function getList() {
+    public function getList(): array {
         return $this->arrayNumbers;
     }
-    public function getNumber(int $index) {
+    public function getNumberByIndex(int $index): int {
         return $this->arrayNumbers[$index];
+    }
+    public function getRandomNumber(): int {
+        $index = array_rand($this->arrayNumbers);
+        return $this->arrayNumbers[$index];
+    }
+    public function isDividedBy3(int $number): bool {
+        return $number % 3 === 0;
+    }
+
+    public function isDividedBy5(int $number): bool {
+        return $number % 5 === 0;
+    }
+
+    public function isDividedBy3And5(int $number): bool {
+        return ($number % 3 === 0 && $number % 5 === 0);
+    }
+    public function convertToText(int $number): string {
+        if ($this->isDividedBy3And5($number)) {
+            return 'FizzBuzz';
+        }elseif ($this->isDividedBy3($number)) {
+            return 'Fizz';
+        }elseif ($this->isDividedBy5($number)) {
+            return 'Buzz';
+        }
+        return $number;
+    }
+
+    public function showFizzBuzzList() {
+
+        return array_map([$this, 'convertToText'], $this->arrayNumbers);
+
     }
 
 
--- tests/Unit/FizzBuzzTest.php
+++ tests/Unit/FizzBuzzTest.php
@@ -7,69 +7,280 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-
-    public function test_fizzbuzz_is_an_object(): void {
-        $fzbz = new FizzBuzz(range(1, 20));
-        $this->assertIsObject($fzbz);
-    }
-
-    public function test_fizzbuzz_arraynumbers_is_an_array(): void {
-        $fzbz = new FizzBuzz(range(1, 20));
-        $this->assertIsArray($fzbz->arrayNumbers);
-    }
-
-
-    public function test_get_1_when_value_is_1(): void
+    /**
+     * Contendrá una instancia de la clasa FizzBuzz, que es una implementación del juego FizzBuzz.
+     * FizzBuzz es un juego que consiste en reemplazar los numeros divisible entre 3 con
+     * la palabra "Fizz", los numeros divisible entre 5 se reemplazaran con la palabra "Buzz" y
+     * los numeros divisible entre 3 y tambien divisible entre 5 con la palabra "FizzBuzz"
+     *
+     * @var FizzBuzz
+     */
+    private $fzbz;
+    /**
+     * Se prepara el contexto de entorno para las pruebas de FizzBuzz.
+     * Se crea una instancia de la clase FizzBuzz y se genera una lista de números del 1 al 100.
+     *
+     * @return void
+     */
+    protected function setUp(): void
     {
-        $fzbz = new FizzBuzz(range(1, 20));
-
-        $list = $fzbz->getList();
-
+        parent::setUp();
+        $this->fzbz = new FizzBuzz(range(1, 100));
+    }
+    /**
+     * @test the fizzbuzz class is an object
+     * Comprueba que la instancia de la clase FizzBuzz sea un objeto.
+     * @return void
+     */
+    public function test_the_fizzbuzz_class_is_an_object(): void
+    {
+        $this->assertIsObject($this->fzbz);
+    }
+    /**
+     * @test the arrayNumbers property is an array
+     * Verifica que la propiedad $arrayNumbers sea un array.
+     * @return void
+     */
+    public function test_the_arrayNumbers_prop_is_an_array(): void
+    {
+        $this->assertIsArray($this->fzbz->arrayNumbers);
+    }
+    /**
+     * @test the first element is 1 from the list
+     * Comprueba que el primer elemento de la lista sea el numero 1
+     * @return void
+     */
+    public function test_the_first_element_is_1_from_the_list(): void
+    {
+        $list = $this->fzbz->getList();
         $this->assertEquals(1, $list[0]);
     }
-
-    public function test_get_the_value_3_from_the_list(): void
+    /**
+     * @test the third element is 3 from the list
+     * Comprueba que el tercer elemento de la lista sea el numero 3
+     * @return void
+     */
+    public function test_the_third_element_is_3_from_the_list(): void
     {
-        $fzbz = new FizzBuzz(range(1, 20));
-
-        $list = $fzbz->getList();
-
+        $list = $this->fzbz->getList();
         $this->assertEquals(3, $list[2]);
     }
-
-    public function test_get_the_value_5_from_the_list(): void
+    /**
+     * @test the fifth element is 5 from the list
+     * Comprueba que el quinto elemento de la lista sea el numero 5
+     * @return void
+     */
+    public function test_the_fifth_element_is_5_from_the_list(): void
     {
-        $fzbz = new FizzBuzz(range(1, 20));
-
-        $list = $fzbz->getList();
-
+        $list = $this->fzbz->getList();
         $this->assertEquals(5, $list[4]);
     }
-
-    public function test_get_any_value_from_the_list_by_index(): void
+    /**
+     * @test get a random number from the list
+     * Comprueba el obtener un numero aleatorio de la lista.
+     * @return void
+     */
+    public function test_get_a_random_number_from_the_list(): void
     {
-        $fzbz = new FizzBuzz(range(1, 20));
-
-        $number1 = $fzbz->getNumber(random_int(0, count($fzbz->arrayNumbers) - 1));
-        $number2 = $fzbz->getNumber(random_int(0, count($fzbz->arrayNumbers) - 1));
-
-        $this->assertContains($number1, $fzbz->arrayNumbers);
-        $this->assertContains($number2, $fzbz->arrayNumbers);
+        $number1 = $this->fzbz->getRandomNumber();
+        $this->assertContains($number1, $this->fzbz->arrayNumbers);
     }
+    /**
+     * @test get any number from the list by index
+     * Comprueba el obtener un numero cualquiera de la lista por medio del indice.
+     * @return void
+     */
+    public function test_get_any_number_from_the_list_by_index(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(21);
+        $this->assertContains($number1, $this->fzbz->arrayNumbers);
+    }
+    /**
+     * @test if the number is divided by 3
+     * Comprueba si el numero es divisible entre 3.
+     * @return void
+     */
+    public function test_if_the_number_is_divided_by_3(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(2);
+        $this->assertTrue($this->fzbz->isDividedBy3($number1));
+    }
+    /**
+     * @test if the number is not divided by 3
+     * Comprueba si el numero no es divisible entre 3.
+     * @return void
+     */
+    public function test_if_the_number_is_not_divided_by_3(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(6);
+        $this->assertFalse($this->fzbz->isDividedBy3($number1));
+    }
+    /**
+     * @test if a random number is divided by 3 or not
+     * Comprueba si un numero aleatorio es divisible entre 3 o no.
+     * Esta prueba puede fallar debido a que el numero a comprobar
+     * es aleatorio.
+     * @return void
+     */
+    public function test_if_a_random_number_is_divided_by_3_or_not(): void
+    {
+        $number1 = $this->fzbz->getRandomNumber();
+        $this->assertTrue($this->fzbz->isDividedBy3($number1));
+    }
+    /**
+     * @test if the number is divided by 5
+     * Comprueba si el numero es divisible entre 5.
+     * @return void
+     */
+    public function test_if_the_number_is_divided_by_5(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(4);
+        $this->assertTrue($this->fzbz->isDividedBy5($number1));
+    }
+    /**
+     * @test if the number is not divided by 5
+     * Comprueba si el numero no es divisible entre 5.
+     * @return void
+     */
+    public function test_if_the_number_is_not_divided_by_5(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(22);
+        $this->assertFalse($this->fzbz->isDividedBy5($number1));
+    }
+    /**
+     * @test if a random number is divided by 5 or not
+     * Comprueba si un numero aleatorio es divisible entre 5 o no.
+     * Esta prueba puede fallar debido a que el numero a comprobar
+     * es aleatorio.
+     * @return void
+     */
+    public function test_if_a_random_number_is_divided_by_5_or_not(): void
+    {
+        $number1 = $this->fzbz->getRandomNumber();
+        $this->assertTrue($this->fzbz->isDividedBy5($number1));
+    }
+    /**
+     * @test if the number is divided by 3 and divide by 5
+     * Comprueba si el numero es divisible entre 3 y es divisible entre 5.
+     * @return void
+     */
+    public function test_if_the_number_is_divided_by_3_and_divided_by_5(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(14);
+        $this->assertTrue($this->fzbz->isDividedBy3And5($number1));
+    }
+    /**
+     * @test if the number is not divided by 3 and not divided by 5
+     * Comprueba si el numero no es divisible entre 3 y no es divisible entre 5.
+     * @return void
+     */
+    public function test_if_the_number_is_not_divided_by_3_and_not_divided_by_5(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(27);
+        $this->assertFalse($this->fzbz->isDividedBy3And5($number1));
+    }
+    /**
+     * @test if a random number is divided by 3 and divided by 5 or not
+     * Comprueba si un numero aleatorio es divisible entre 3 o no.
+     * Esta prueba puede fallar debido a que el numero a comprobar
+     * es aleatorio.
+     * @return void
+     */
+    public function test_if_the_random_number_is_divided_by_3_and_divided_by_5_or_not(): void
+    {
+        $number1 = $this->fzbz->getRandomNumber();
+        $this->assertTrue($this->fzbz->isDividedBy3And5($number1));
+    }
+    /**
+     * @test when the number is divided by 3 replace with fizz
+     * Verifica cuando un numero es divisible entre 3 reemplazarlo con el texto "Fizz"
+     * @return void
+     */
+    public function test_when_the_number_is_divided_by_3_replace_with_fizz(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(2);
+        $text1 = $this->fzbz->convertToText($number1);
+        $this->assertEquals('Fizz', $text1);
+    }
+    /**
+     * @test when the number is divided by 5 replace with buzz
+     * Verifica cuando un numero es divisible entre 5 reemplazarlo con el texto "Buzz"
+     * @return void
+     */
+    public function test_when_the_number_is_divided_by_5_replace_with_buzz(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(4);
+        $text1 = $this->fzbz->convertToText($number1);
+        $this->assertEquals('Buzz', $text1);
+    }
+    /**
+     * @test when the number is divided by 3 and divided by 5 replace with fizzbuzz
+     * Verifica cuando un numero es divisible entre 3 y es divisible entre 5 reemplazarlo con el texto "FizzBuzz"
+     * @return void
+     */
+    public function test_when_the_number_is_divided_by_3_and_divide_by_5_replace_with_fizzbuzz(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(14);
+        $text1 = $this->fzbz->convertToText($number1);
+        $this->assertEquals('FizzBuzz', $text1);
+    }
+    /**
+     * @test when the number is not divided by 3 and is not divided by 5 return the number
+     * Verifica cuando un numero no es divisible entre 3 y no es divisible entre 5, retorna el numero
+     * @return void
+     */
+    public function test_when_the_number_is_not_divided_by_3_and_is_not_divided_by_5_return_the_number(): void
+    {
+        $number1 = $this->fzbz->getNumberByIndex(12);
+        $text1 = $this->fzbz->convertToText($number1);
+        $this->assertNotEquals('Fizz', $text1);
+        $this->assertNotEquals('Buzz', $text1);
+        $this->assertNotEquals('FizzBuzz', $text1);
+    }
+    /**
+     * @test the list is an array
+     * Comprueba la lista resultante es un array
+     * @return void
+     */
+    public function test_the_list_is_an_array(): void {
+        $list = $this->fzbz->showFizzBuzzList();
+        $this->assertIsArray($list);
 
-    // public function test_convert_fizz_when_value_is_divided_by_3(): void
-    // {
-    //     $fzbz = new FizzBuzz(range(1, 20));
-
-    //     $number1 = $fzbz->getNumber(2);
-    //     $number2 = $fzbz->getNumber(6);
-    //     $number3 = $fzbz->getNumber(11);
-    //     $number4 = $fzbz->getNumber(16);
-
-    //     $this->assertEquals(3, $list[2]);
-    //     $this->assertEquals(3, $list[2]);
-    //     $this->assertEquals(3, $list[2]);
-    //     $this->assertEquals(3, $list[2]);
-    // }
-
+    }
+    /**
+     * @test the list has 100 numbers
+     * Comprueba la lista resultante tiene 100 numeros
+     * @return void
+     */
+    public function test_the_list_has_100_numbers(): void {
+        $list = $this->fzbz->showFizzBuzzList();
+        $this->assertCount(100, $list);
+    }
+    /**
+     * @test the list has Fizz text
+     * Comprueba la lista resultante tiene el texto Fizz
+     * @return void
+     */
+    public function test_the_list_has_Fizz_text(): void {
+        $list = $this->fzbz->showFizzBuzzList();
+        $this->assertContains('Fizz', $list);
+    }
+    /**
+     * @test the list has Buzz text
+     * Comprueba la lista resultante tiene el texto Buzz
+     * @return void
+     */
+    public function test_the_list_has_Buzz_text(): void {
+        $list = $this->fzbz->showFizzBuzzList();
+        $this->assertContains('Buzz', $list);
+    }
+    /**
+     * @test the list has FizzBuzz text
+     * Comprueba la lista resultante tiene el texto FizzBuzz
+     * @return void
+     */
+    public function test_the_list_has_FizzBuzz_text(): void {
+        $list = $this->fzbz->showFizzBuzzList();
+        $this->assertContains('FizzBuzz', $list);
+    }
 }
